910c51493f4f59e7c5de25010c85c2a9ce4ab5b1
Craig Petzel <craig@lumoslabs.com>
2015-02-24 17:38:56 -0800

    Adding App and Game data to the test result

--- spoon-runner/src/main/java/com/squareup/spoon/DeviceTestResult.java
+++ spoon-runner/src/main/java/com/squareup/spoon/DeviceTestResult.java
@@ -14,9 +14,11 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import com.android.ddmlib.logcat.LogCatMessage;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.squareup.spoon.html.HtmlAppData.GameTestData;
 import com.squareup.spoon.html.HtmlAppData.KeyValuePair;
 import com.squareup.spoon.misc.StackTrace;
 
@@ -37,9 +39,11 @@ public final class DeviceTestResult {
     private final List<KeyValuePair> splitTestAssignments;
     private final List<KeyValuePair> userData;
     private final List<KeyValuePair> serverData;
+    private final List<GameTestData> gameTests;
 
     private DeviceTestResult(Status status, StackTrace exception, long duration, List<File> screenshots, File animatedGif,
-        List<LogCatMessage> log, List<KeyValuePair> splitTestAssignments, List<KeyValuePair> userData, List<KeyValuePair> serverData) {
+        List<LogCatMessage> log, List<KeyValuePair> splitTestAssignments, List<KeyValuePair> userData, List<KeyValuePair> serverData,
+        List<GameTestData> tests) {
         this.status = status;
         this.exception = exception;
         this.duration = duration;
@@ -49,6 +53,7 @@ public final class DeviceTestResult {
         this.splitTestAssignments = splitTestAssignments;
         this.userData = userData;
         this.serverData = serverData;
+        this.gameTests = tests;
     }
 
     /** Execution status. */
@@ -92,6 +97,10 @@ public final class DeviceTestResult {
         return userData;
     }
 
+    public List<GameTestData> getGameTestData() {
+        return gameTests;
+    }
+
     public static class Builder {
         private final List<File> screenshots = new ArrayList<File>();
         private Status status = Status.PASS;
@@ -103,6 +112,7 @@ public final class DeviceTestResult {
         private List<KeyValuePair> splitTestAssignments;
         private List<KeyValuePair> userData;
         private List<KeyValuePair> serverData;
+        private List<GameTestData> gameTests;
 
         public Builder markTestAsFailed(String message) {
             checkNotNull(message);
@@ -169,7 +179,7 @@ public final class DeviceTestResult {
                 log = Collections.emptyList();
             }
             return new DeviceTestResult(status, exception, duration, screenshots, animatedGif, log, splitTestAssignments, userData,
-                serverData);
+                serverData, gameTests);
         }
 
         private void parseAppData(String JsonDataFromTest) {
@@ -177,40 +187,79 @@ public final class DeviceTestResult {
             userData = new LinkedList<KeyValuePair>();
             serverData = new LinkedList<KeyValuePair>();
             splitTestAssignments = new LinkedList<KeyValuePair>();
+            gameTests = new LinkedList<GameTestData>();
 
             JsonParser parser = new JsonParser();
             JsonObject rootJson = (JsonObject) parser.parse(JsonDataFromTest);
 
-            // user data
-            JsonObject userDataJsonObject = rootJson.getAsJsonObject("user");
-            Set<Entry<String, JsonElement>> userKeys = userDataJsonObject.entrySet();
-            for (Entry<String, JsonElement> entry : userKeys) {
-                String name = entry.getKey();
-                JsonElement value = entry.getValue();
-                String splitValue = value.getAsString();
-                userData.add(new KeyValuePair(name, splitValue));
+            try {
+                // user data
+                JsonObject userDataJsonObject = rootJson.getAsJsonObject("user");
+                Set<Entry<String, JsonElement>> userKeys = userDataJsonObject.entrySet();
+                for (Entry<String, JsonElement> entry : userKeys) {
+                    String name = entry.getKey();
+                    JsonElement value = entry.getValue();
+                    String splitValue = value.getAsString();
+                    userData.add(new KeyValuePair(name, splitValue));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
-            // server data
-            JsonObject serverDataJsonObject = rootJson.getAsJsonObject("server");
-            Set<Entry<String, JsonElement>> serverKeys = serverDataJsonObject.entrySet();
-            for (Entry<String, JsonElement> entry : serverKeys) {
-                String name = entry.getKey();
-                JsonElement value = entry.getValue();
-                String splitValue = value.getAsString();
-                serverData.add(new KeyValuePair(name, splitValue));
+            try {
+                // server data
+                JsonObject serverDataJsonObject = rootJson.getAsJsonObject("server");
+                Set<Entry<String, JsonElement>> serverKeys = serverDataJsonObject.entrySet();
+                for (Entry<String, JsonElement> entry : serverKeys) {
+                    String name = entry.getKey();
+                    JsonElement value = entry.getValue();
+                    String splitValue = value.getAsString();
+                    serverData.add(new KeyValuePair(name, splitValue));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
-            // split tests
-            JsonObject splitTestAssignmentJsonObject = rootJson.getAsJsonObject("split_test_assignments");
-            Set<Entry<String, JsonElement>> keys = splitTestAssignmentJsonObject.entrySet();
-            for (Entry<String, JsonElement> entry : keys) {
-                String name = entry.getKey();
-                JsonElement value = entry.getValue();
-                String splitValue = value.getAsString();
-                splitTestAssignments.add(new KeyValuePair(name, splitValue));
+            try {
+                // split tests
+                JsonObject splitTestAssignmentJsonObject = rootJson.getAsJsonObject("split_test_assignments");
+                Set<Entry<String, JsonElement>> keys = splitTestAssignmentJsonObject.entrySet();
+                for (Entry<String, JsonElement> entry : keys) {
+                    String name = entry.getKey();
+                    JsonElement value = entry.getValue();
+                    String splitValue = value.getAsString();
+                    splitTestAssignments.add(new KeyValuePair(name, splitValue));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
+            try {
+                // game app data
+                JsonObject gameDataJson = rootJson.getAsJsonObject("game");
+
+                JsonArray tests = gameDataJson.getAsJsonArray("tests");
+
+                for (JsonElement test : tests) {
+                    JsonObject testObject = test.getAsJsonObject();
+                    int time = 0;
+                    String interactionType = null;
+                    String locale = "Hi Breanna";
+                    try {
+                        time = testObject.get("time").getAsInt();
+                        interactionType = testObject.get("interactionType").getAsString();
+                        locale = testObject.get("locale").getAsString();
+                    } catch (Exception e) {
+                        System.out.println("test info does not contain all data");
+                    }
+                    String assertMessage = testObject.get("assertMessage").getAsString();
+                    String status = testObject.get("status").getAsString();
+                    String testType = testObject.get("test").getAsString();
+                    gameTests.add(new GameTestData(time, assertMessage, interactionType, status, testType, locale));
+                }
+            } catch (Exception e) {
+                System.out.println("No Game or Test Json object in results");
+            }
         }
     }
 }
